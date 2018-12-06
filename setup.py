92cfd275a0a5d0354c47a42fd805bf8d6719842c
kenkov <kenkovtan@gmail.com>
2018-12-06 22:26:03 +0900

    依存ライブラリを追加

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ setup(
     name="estimoji",
     packages=["estimoji"],
     package_data={"estimoji": ["meoji_id.txt"]},
-    install_requires=["emoji==0.5.0"],
+    install_requires=[
+        "emoji==0.5.0",
+        "scikit-learn==0.19.2"
+        ],
     version="0.1.0",
     author="kenkov",
     author_email="kenkovtan@gmail.com",
